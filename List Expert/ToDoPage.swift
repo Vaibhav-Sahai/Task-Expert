22f3766db5ef6b66f182de5f96f17517c2445907
Vaibhav <vaibhavsahai2003@gmail.com>
2021-04-18 18:40:42 +0400

    Added Navigation View

---
+++ List Expert/ToDoPage.swift
@@ -0,0 +1,21 @@
+//
+//  ToDoPage.swift
+//  List Expert
+//
+//  Created by Vaibhav Sahai on 18/04/2021.
+//
+
+import SwiftUI
+
+struct ToDoPage: View {
+    var body: some View {
+        Text("Testy")
+            .navigationBarBackButtonHidden(true)
+    }
+}
+
+struct ToDoPage_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoPage()
+    }
+}

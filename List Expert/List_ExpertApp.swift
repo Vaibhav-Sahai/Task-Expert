1548adf9ddf3542d608dc96a476ae22cd97a8747
Vaibhav <vaibhavsahai2003@gmail.com>
2021-04-18 14:42:31 +0400

    Initial Commit

---
+++ List Expert/List_ExpertApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_ExpertApp.swift
+//  List Expert
+//
+//  Created by Vaibhav Sahai on 18/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct List_ExpertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

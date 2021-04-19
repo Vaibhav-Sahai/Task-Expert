56c064ba31bea0b68d2b6ca1ad23ce20e4b3adb8
Vaibhav <vaibhavsahai2003@gmail.com>
2021-04-19 14:01:55 +0400

    Added Basics of To Do Page

--- List Expert/ToDoPage.swift
+++ List Expert/ToDoPage.swift
@@ -9,8 +9,41 @@ import SwiftUI
 
 struct ToDoPage: View {
     var body: some View {
-        Text("Testy")
-            .navigationBarBackButtonHidden(true)
+        ZStack{
+            //MARK: - Putting Banner on Top
+            VStack{
+                Image("Banner")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .frame(width: 500 ,height: 100)
+                Spacer()
+            }
+            //MARK:- Date Time And List Button
+            VStack{
+                HStack(spacing: 50){
+                    Text("31st September, 2021")
+                        .font(.system(size: 24, weight: .semibold, design: .default))
+                        .minimumScaleFactor(0.25)
+                        .lineLimit(1)
+                        .allowsTightening(true)
+                        .padding()
+                    
+                    Button(action: {
+                        //Add List Stuff Here
+                    }) {
+                        Image(systemName: "text.insert")
+                            .renderingMode(.original)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 30, height: 30)
+                            .padding()
+                    }
+                }
+                Spacer()
+            }
+        }
+        .navigationBarBackButtonHidden(true)
+        .navigationBarHidden(true)
     }
 }
 

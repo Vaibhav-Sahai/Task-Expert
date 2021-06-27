947f2b804c8e2cdecd2903181e12ec693604cf22
Vaibhav <vaibhavsahai2003@gmail.com>
2021-06-27 19:01:03 +0400

    Removed Bad Popup Controller

--- List Expert/ToDoPage.swift
+++ List Expert/ToDoPage.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ToDoPage: View {
     @State var dateAndTime = "" //Init Identifier
-    @State var isPresentingAddModal = false
+    
     var body: some View {
         ZStack{
             //MARK: - Putting Banner on Top
@@ -32,7 +32,7 @@ struct ToDoPage: View {
                 
                     Button(action: {
                         //Toggle Modal Popup View
-                        self.isPresentingAddModal.toggle()
+                        
                     }) {
                         Image(systemName: "text.insert")
                             .renderingMode(.original)
@@ -42,10 +42,7 @@ struct ToDoPage: View {
                             .padding()
                     }
                 }
-                //Show Modal Popup View
-                .fullScreenCover(isPresented: $isPresentingAddModal, content: {
-                    AddModal(isPresented: self.$isPresentingAddModal)
-                })
+                
                 Spacer()
             }
         }.onAppear{
@@ -61,55 +58,6 @@ struct ToDoPage: View {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd MMMM, hh:mm a"
         dateAndTime = dateFormatter.string(from: currentDate)
-    }
-}
-//MARK:- Modal Popup View
-struct AddModal: View{
-    //Var Declarations
-    @Binding var isPresented: Bool
-    @State var listName = ""
-    
-    var body: some View{
-        ZStack{
-            VStack(spacing: 40){
-
-                Text("Create New List")
-                    .font(.system(size: 42, weight: .semibold, design: .default))
-                    .minimumScaleFactor(0.25)
-                    .lineLimit(1)
-                    .allowsTightening(true)
-                    .padding()
-                
-                TextField("Enter List Name", text: $listName)
-                    .frame(width: 280, height: 50)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-
-                //Add And Cancel Buttons
-                VStack(spacing: 10){
-                    Button(action: {
-                        
-                    }, label: {
-                        Text("Add")
-                            .frame(width: 280, height: 50)
-                            .foregroundColor(.white)
-                            .background(Color.blue)
-                            .cornerRadius(10)
-                            .shadow(color: Color.blue, radius: 4)
-                    })
-                    Button(action: {
-                        self.isPresented.toggle()
-                    }, label: {
-                        Text("Cancel")
-                            .frame(width: 280, height: 50)
-                            .foregroundColor(.white)
-                            .background(Color.red)
-                            .cornerRadius(10)
-                            .shadow(color: Color.red, radius: 4)
-                })
-                }
-            }.padding()
-        }
     }
 }
 

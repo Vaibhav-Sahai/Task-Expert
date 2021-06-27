c393b8d6bf3695c2c04b3e2dac12c446b41ac98f
Vaibhav <vaibhavsahai2003@gmail.com>
2021-06-27 19:09:08 +0400

    Added New Add Task Controller

---
+++ List Expert/AddTask.swift
@@ -0,0 +1,20 @@
+//
+//  AddTask.swift
+//  List Expert
+//
+//  Created by Vaibhav Sahai on 27/06/2021.
+//
+
+import SwiftUI
+
+struct AddTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddTask_Previews: PreviewProvider {
+    static var previews: some View {
+        AddTask()
+    }
+}

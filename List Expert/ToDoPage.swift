6c54997f60e9d933c954e7acd1faaaeaa7faabf1
Vaibhav <vaibhavsahai2003@gmail.com>
2021-06-28 18:20:32 +0400

    Added Vertical Grid

--- List Expert/ToDoPage.swift
+++ List Expert/ToDoPage.swift
@@ -7,8 +7,28 @@
 
 import SwiftUI
 
+//MARK:- Cells For Task Managers
+struct Task: Hashable{
+    var id: Int
+    let title, image, remaining: String
+    let colorIcon, colorCell: Color
+    
+}
+
 struct ToDoPage: View {
+
+    var columns: [GridItem] =
+        Array(repeating: .init(.flexible(maximum: 160), spacing: 35, alignment: .center), count: 2)
+
     @State var dateAndTime = "" //Init Identifier
+    
+    //MARK:- Assigning Data to Cells
+    let Tasks: [Task] = [
+        Task(id: 0, title: "Urgent Tasks", image: "exclamationmark", remaining: "0", colorIcon: Color("LightRed"), colorCell: .red),
+        Task(id: 1, title: "Work Tasks", image: "bag.fill", remaining: "0", colorIcon: Color("LightBlue"), colorCell: .blue),
+        Task(id: 2, title: "Shopping Tasks", image: "cart", remaining: "0", colorIcon: Color("LightGreen"), colorCell: .green),
+        Task(id: 3, title: "Miscellaneous Tasks", image: "pencil", remaining: "0", colorIcon: Color("LightGray"), colorCell: .gray)
+    ]
     
     var body: some View {
         ZStack{
@@ -20,8 +40,8 @@ struct ToDoPage: View {
                     .frame(width: 500 ,height: 100)
                 Spacer()
             }
-            //MARK:- Date Time And List Button
-            VStack{
+            //MARK:- Date Time
+            VStack(spacing: 10){
                 HStack(spacing: 70){
                     Text(dateAndTime)
                         .font(.system(size: 24, weight: .semibold, design: .default))
@@ -29,21 +49,20 @@ struct ToDoPage: View {
                         .lineLimit(1)
                         .allowsTightening(true)
                         .padding()
-                    
-                    NavigationLink(
-                        destination: AddTask(),
-                        label: {
-                            Image(systemName: "text.insert")
-                                .renderingMode(.original)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 30, height: 30)
-                                .padding()
-                        })
                 }
-                
                 Spacer()
+                //MARK:- Calling TaskView
+                VStack {
+                    ScrollView{
+                        LazyVGrid(columns: columns, spacing: 20){
+                            ForEach(Tasks, id: \.self){ Task in
+                                TaskView(task: Task)
+                            }
+                        }.padding()
+                    }.padding()
+                }
             }
+
         }.onAppear{
             //Running Date Call
             self.dateFormatterTool()
@@ -59,9 +78,58 @@ struct ToDoPage: View {
         dateAndTime = dateFormatter.string(from: currentDate)
     }
 }
+//MARK:- How Each Task Looks
+struct TaskView: View {
+    let task: Task
+    
+    var body: some View{
+        NavigationLink(
+            destination: UrgentTaskAdd(),
+            label: {
+                VStack{
+                    Image(systemName: task.image)
+                        .resizable()
+                        .renderingMode(.original)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                        .padding()
+                        .background(task.colorIcon)
+                        .cornerRadius(100)
+                    
+                    VStack(spacing: 10) {
+                        Text(task.title)
+                            .font(.system(size: 15, weight: .semibold, design: .default))
+                            .foregroundColor(.white)
+                            .minimumScaleFactor(0.25)
+                            .lineLimit(1)
+                            .allowsTightening(true)
+                        
+                        Text("Remainging Tasks: " + task.remaining)
+                            .font(.system(size: 11, weight: .light, design: .default))
+                            .foregroundColor(.black)
+                    }
+                }
+                .frame(width: 180, height: 180)
+                .background(task.colorCell)
+                .cornerRadius(50)
+            })
+        
+    }
+}
+/*
+//MARK:- Uncomment To Change Task Cell View
+struct TaskView_Previews: PreviewProvider{
+    static var previews: some View{
+        TaskView()
+    }
+}
+*/
 
 struct ToDoPage_Previews: PreviewProvider {
     static var previews: some View {
         ToDoPage()
+            .previewDevice("iPhone 12 Pro Max")
+        
     }
 }
+
---
+++ List Expert/UrgentTaskAdd.swift
@@ -0,0 +1,20 @@
+//
+//  UrgentTaskAdd.swift
+//  List Expert
+//
+//  Created by Vaibhav Sahai on 28/06/2021.
+//
+
+import SwiftUI
+
+struct UrgentTaskAdd: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UrgentTaskAdd_Previews: PreviewProvider {
+    static var previews: some View {
+        UrgentTaskAdd()
+    }
+}

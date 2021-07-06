c5dc2bc6f85db722e39faa2781e63ec632f2257b
Vaibhav <vaibhavsahai2003@gmail.com>
2021-07-06 20:01:03 +0400

    Added Form View

--- List Expert/TaskAddScreen.swift
+++ List Expert/TaskAddScreen.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct TaskAddScreen: View {
+    //MARK:- Info Taken From Form Here
+    @State var taskBody: String = ""
+    @State var colorScheme: String = "Red"
     let taskType: String
     var body: some View {
-        Text("\(taskType)")
+        Form{
+            Section(header: Text("New Task Information")) {
+                TextField("Task Body", text: $taskBody )
+                Text("Choose Task Color Scheme")
+                Picker("Choose Color", selection: $colorScheme, content: {
+                    Text("Red").tag("Red")
+                    Text("Blue").tag("Blue")
+                    Text("Green").tag("Green")
+                    Text("Grey").tag("Grey")
+                }).pickerStyle(SegmentedPickerStyle())
+            }
+            //NavigationLink
+        }
         .navigationBarBackButtonHidden(true)
-        .navigationBarHidden(true)
+        .navigationBarTitle("Add New Task")
     }
 }
 

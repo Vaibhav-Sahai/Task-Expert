71b671c0573f4eaca66751313d26ee103a1604bb
Vaibhav <vaibhavsahai2003@gmail.com>
2021-07-07 19:56:45 +0400

    Finished Task Add Screen

--- List Expert/TaskAddScreen.swift
+++ List Expert/TaskAddScreen.swift
@@ -13,21 +13,49 @@ struct TaskAddScreen: View {
     @State var colorScheme: String = "Red"
     let taskType: String
     var body: some View {
-        Form{
-            Section(header: Text("New Task Information")) {
-                TextField("Task Body", text: $taskBody )
-                Text("Choose Task Color Scheme")
-                Picker("Choose Color", selection: $colorScheme, content: {
-                    Text("Red").tag("Red")
-                    Text("Blue").tag("Blue")
-                    Text("Green").tag("Green")
-                    Text("Grey").tag("Grey")
-                }).pickerStyle(SegmentedPickerStyle())
+        VStack {
+            //Info Form
+            Form{
+                Section(header: Text("New Task Information")) {
+                    TextField("Task Body", text: $taskBody )
+                    Text("Choose Task Color Scheme")
+                    Picker("Choose Color", selection: $colorScheme, content: {
+                        Text("Red").tag("Red")
+                        Text("Blue").tag("Blue")
+                        Text("Green").tag("Green")
+                        Text("Grey").tag("Grey")
+                    }).pickerStyle(SegmentedPickerStyle())
+                }
             }
-            //NavigationLink
+            .navigationBarBackButtonHidden(true)
+            .navigationBarTitle("Add New Task")
+            
+            //MARK:- Add Task
+            VStack(spacing: 10) {
+                NavigationLink(
+                    destination: individualTaskView(task: individualTask.init(id: 2, title: "", color1: "", color2: "")),
+                    label: {
+                        Text("Add Task")
+                            .frame(width: 280, height: 50)
+                            .foregroundColor(.white)
+                            .background(Color("TaskBlue2"))
+                            .cornerRadius(10)
+                            .shadow(color: .black, radius: 4)
+                })
+            //MARK:- Don't Add Task
+                NavigationLink(
+                    destination: individualTaskView(task: individualTask.init(id: 2, title: "", color1: "", color2: "")),
+                    label: {
+                        Text("Cancel")
+                            .frame(width: 280, height: 50)
+                            .foregroundColor(.white)
+                            .background(Color("TaskRed2"))
+                            .cornerRadius(10)
+                            .shadow(color: .black, radius: 4)
+                    })
+            }
+            
         }
-        .navigationBarBackButtonHidden(true)
-        .navigationBarTitle("Add New Task")
     }
 }
 
